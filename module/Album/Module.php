3cefc4fa9a162183a6c33e68d534a7bd7f37df63
Golars <titorens@gmail.com>
2013-10-07 23:19:16 +0300

    +Fix Album Module

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -1,10 +1,15 @@
 <?php
+
 namespace Album;
 
-class Module
-{
-    public function getAutoloaderConfig()
-    {
+use Album\Model\Album;
+use Album\Model\AlbumTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+class Module {
+
+    public function getAutoloaderConfig() {
         return array(
             'Zend\Loader\ClassMapAutoloader' => array(
                 __DIR__ . '/autoload_classmap.php',
@@ -17,8 +22,27 @@ class Module
         );
     }
 
-    public function getConfig()
-    {
+    public function getConfig() {
         return include __DIR__ . '/config/module.config.php';
     }
+
+    public function getServiceConfig() {
+        return array(
+            'factories' => array(
+                'Album\Model\AlbumTable' => function($sm) {
+                    $tableGateway = $sm->get('AlbumTableGateway');
+                    $table = new AlbumTable($tableGateway);
+                    return $table;
+                },
+                'AlbumTableGateway' => function ($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Album());
+                    return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+                },
+            ),
+        );
+    }
+
+
 }
